b004c3806bee49403753db7484b0c81c1c43d420
hasanfurkanfidan <furkanfidan.job@gmail.com>
2021-02-21 21:31:58 +0300

    Add project files.

---
+++ Core/DataAccess/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    public interface IGenericRepository<TEntity> where TEntity : class, IEntity, new()
+    {
+        List<TEntity> GetList(Expression<Func<TEntity,bool>>expression=null);
+        TEntity GetById(int id);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
